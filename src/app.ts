9f605c3a5ae1cd24910f3a0ad53210cfb5cc78f8
Bartłomiej Banach <b.banach@benefitsystems.pl>
2025-02-06 15:06:31 +0100

    build: cleanup

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,6 @@
 import express from "express";
 import logger from "morgan";
 import * as path from "path";
-
-import { errorHandler, errorNotFoundHandler } from "./middlewares/errorHandler";
-
-// Routes
-import { index } from "./routes/index";
-// Create Express server
 export const app = express();
 
 // Express configuration
@@ -17,7 +11,6 @@ app.set("view engine", "pug");
 app.use(logger("dev"));
 
 app.use(express.static(path.join(__dirname, "../public")));
-app.use("/", index);
-
-app.use(errorNotFoundHandler);
-app.use(errorHandler);
+app.use("/", (req, res) => {
+    res.send("Hello World!");
+});
