f83012131ffbcb80ad2de389529fda43b18ba793
Bartłomiej Banach <b.banach@benefitsystems.pl>
2025-02-06 15:20:35 +0100

    refactor: remove stuff

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,6 @@ export const app = express();
 
 // Express configuration
 app.set("port", process.env.PORT || 3000);
-app.set("views", path.join(__dirname, "../views"));
-app.set("view engine", "pug");
-
 app.use(logger("dev"));
 
 app.use(express.static(path.join(__dirname, "../public")));
