2368f3f9547b84ede97be828fddb0694b41679b5
Bartłomiej Banach <b.banach@benefitsystems.pl>
2025-10-01 16:22:42 +0200

    fix: small fixes

--- src/features/generateName.ts
+++ src/features/generateName.ts
@@ -1,24 +1,26 @@
-import { count, eq } from "drizzle-orm";
+import { count } from "drizzle-orm";
 import { db } from "../database/connection";
 import { adjective, plant } from "../database/schema";
 
 const getRandomAdjective = async () => {
     const [result] = await db.select({ count: count() }).from(adjective);
-    const randomIndex = Math.floor(Math.random() * result.count);
-    const adj = await db
-        .select()
-        .from(adjective)
-        .where(eq(adjective.id, randomIndex));
+    const randomIndex = Math.floor(Math.random() * (result.count - 1));
+
+    console.log(randomIndex);
+
+    const adj = await db.select().from(adjective).offset(randomIndex).limit(1);
     return adj[0];
 };
 
 const getRandomPlant = async () => {
     const [result] = await db.select({ count: count() }).from(plant);
-    const randomIndex = Math.floor(Math.random() * result.count);
+
+    const randomIndex = Math.floor(Math.random() * (result.count - 1));
+
     const randomPlant = await db
         .select()
         .from(plant)
-        .offset(randomIndex - 1)
+        .offset(randomIndex)
         .limit(1);
     return randomPlant[0];
 };
@@ -38,6 +40,8 @@ const mapGenders = (gender: "f" | "m" | "n" | null) => {
 export const generateName = async () => {
     const plant = await getRandomPlant();
     const adj = await getRandomAdjective();
+
+    console.log(plant, adj, mapGenders(plant.gender));
     const genderedAdjective = adj[mapGenders(plant.gender)];
 
     return `${genderedAdjective}_${plant.name}`;
